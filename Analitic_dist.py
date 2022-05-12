6e49bd08180476d718c12b4e20fc3b8e658db987
Stepan Tikhomirov <63906533+Tihstep@users.noreply.github.com>
2022-05-12 13:47:49 +0300

    Update Analitic_dist.py

--- Analitic_dist.py
+++ Analitic_dist.py
@@ -6,10 +6,10 @@ def combinations(n, k):
     if 0 <= k <= n:
         nn = 1
         kk = 1
-    for t in range(1, min(k, n - k) + 1):
-        nn *= n
-        kk *= t
-        n -= 1
+        for t in range(1, min(k, n - k) + 1):
+            nn *= n
+            kk *= t
+            n -= 1
         return nn // kk
     else:
         return 0
